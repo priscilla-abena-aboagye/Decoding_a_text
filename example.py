8961d9a1c6ccce01018e2323f954c5f16a6d89ec
Abena <priscillaabenaaboagye@gmail.com>
2025-01-07 21:32:03 +0000

    practicing python

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
 myName = "Priscilla Abena Aboagye"
 shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+encrypted_text = ""
 
 # print(shifted)
 
 for char in myName.lower():
      index = alphabet.find(char)
      new_index = index + shift
-     new_char = alphabet[new_index]
-     print(new_char)
+     encrypted_text = encrypted_text + alphabet[new_index]
+     print(f"char: {char} encrypted text: {encrypted_text}")
