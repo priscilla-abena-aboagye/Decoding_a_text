d6f64f0b97fa2b77b4012394074d14fdb41665ab
Abena <priscillaabenaaboagye@gmail.com>
2025-01-16 20:49:59 +0000

    Update example.py

--- example.py
+++ example.py
@@ -3,13 +3,12 @@ shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 encrypted_text = ""
 
-# print(shifted)
 
 for char in myName.lower():
      if char == " ":
           encrypted_text += char
      else: 
          index = alphabet.find(char)
-         new_index = (index + shift) % 26
+         new_index = (index + shift) % len(alphabet)
          encrypted_text +=alphabet[new_index]
      print(f"char: {char} encrypted text: {encrypted_text}")
