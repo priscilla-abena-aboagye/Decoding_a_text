3b567723a3c573ce11f927909407120b416a2f37
Abena <priscillaabenaaboagye@gmail.com>
2025-01-20 15:24:03 +0000

    explaining with comment

--- example.py
+++ example.py
@@ -1,30 +1,35 @@
 myName = "Priscilla Abena Aboagye"
 shift = 3
+custom_key = "python"
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 
 def caesar(message, changed):
     encrypted_text = ""
     
-    for char in message.lower():
+    for char in message.lower(): # it takes every letter in message which is myName
          if char == " ":
-              encrypted_text += char
+              encrypted_text += char # if char is an empty space then add it to the encrpted_text
          else: 
-             index = alphabet.find(char)
+             index = alphabet.find(char) # finds char and places it in alphabet and the number gotten is stored in a variable.
              new_index = (index + changed) % len(alphabet)
              encrypted_text +=alphabet[new_index]
     print(f"Plain Text: {message}")
     print(f"encrypted text: {encrypted_text}")
 
-caesar(myName, shift)
+caesar(myName, shift) # calls the function
 caesar(myName, 10)
 
 
-def vigenere(message, changed):
+
+
+def vigenere(message, key):
+     key_index = 0 # this makes it repeat itself and also makes it start from the first letter
      encrypted_text = ""
      for char in message.lower():
           if char == " ":
                encrypted_text += char
           else:
                index = alphabet.find(char)
-               new_index = alphabet[index + shift]
+               new_index = (index + changed) % len(alphabet)
+               encrypted_text += alphabet[new_index]
