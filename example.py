6836b1d50e250482bfd9ad04fd7f41227c403972
Abena <priscillaabenaaboagye@gmail.com>
2025-01-20 15:08:55 +0000

    Adding vigenere

--- example.py
+++ example.py
@@ -1,15 +1,30 @@
 myName = "Priscilla Abena Aboagye"
 shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-encrypted_text = ""
 
-def caesar():
-    for char in myName.lower():
+
+def caesar(message, changed):
+    encrypted_text = ""
+    
+    for char in message.lower():
          if char == " ":
               encrypted_text += char
          else: 
              index = alphabet.find(char)
-             new_index = (index + shift) % len(alphabet)
+             new_index = (index + changed) % len(alphabet)
              encrypted_text +=alphabet[new_index]
-    print(f"Plain Text: {myName}")
-    print(f"char: {char} encrypted text: {encrypted_text}")
+    print(f"Plain Text: {message}")
+    print(f"encrypted text: {encrypted_text}")
+
+caesar(myName, shift)
+caesar(myName, 10)
+
+
+def vigenere(message, changed):
+     encrypted_text = ""
+     for char in message.lower():
+          if char == " ":
+               encrypted_text += char
+          else:
+               index = alphabet.find(char)
+               new_index = alphabet[index + shift]
