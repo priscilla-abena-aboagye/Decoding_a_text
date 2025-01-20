936b59b8fd61de8f2790074fe9eb831bd1b7a627
Abena <priscillaabenaaboagye@gmail.com>
2025-01-20 19:18:01 +0000

    Update example.py

--- example.py
+++ example.py
@@ -14,8 +14,10 @@ def caesar(message, changed):
              index = alphabet.find(char) # finds char and places it in alphabet and the number gotten is stored in a variable.
              new_index = (index + changed) % len(alphabet)
              encrypted_text +=alphabet[new_index]
+    print("----------------")
     print(f"Plain Text: {message}")
     print(f"encrypted text: {encrypted_text}")
+    print("------------")
 
 caesar(myName, shift) # calls the function
 caesar(myName, 10)
@@ -23,23 +25,32 @@ caesar(myName, 10)
 
 
 
-def vigenere(message, key, direction):
+def vigenere(message, key, direction = 1):
      key_index = 0 # this makes it repeat itself and also makes it start from the first letter
      encrypted_text = ""
      for char in message.lower():
-          if char == " ":
+          if not char.isalpha():
                encrypted_text += char
           else:
                key_char = key[key_index % len(key)]
                # Every time you process a letter in the text, you increase key_index by 1 to use the next letter of the key.
                key_index += 1 
-               # Define the offset and the encrypted letter
+               # Define the offset and the encrypted/decrypted letter
                offset = alphabet.index(key_char) 
                index = alphabet.find(char)
                new_index = (index + offset * direction) % len(alphabet)
                encrypted_text += alphabet[new_index]
      return encrypted_text
-encryption = vigenere(myName, custom_key, 1)
-decryption = vigenere(encryption. custom_key, -1)
-print(encryption)
-print(decryption)
+
+def encrypt(message, key):
+     return vigenere(message, key)
+def decrypt(message, key):
+     return vigenere(message, key, -1)
+
+encryption = encrypt(myName, custom_key)
+decryption = decrypt(encryption, custom_key)
+print("------------")
+print(f"Encrypted text: {encryption}")
+print(f"Key: {custom_key}")
+print(f"Decryted text: {decryption}")
+print("-------------")
