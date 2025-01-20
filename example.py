2f6a02356a4f5bef3372d2ae773321ebf14e1c6a
Abena <priscillaabenaaboagye@gmail.com>
2025-01-20 14:39:11 +0000

    placed it in a function

--- example.py
+++ example.py
@@ -3,13 +3,13 @@ shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 encrypted_text = ""
 
-
-for char in myName.lower():
-     if char == " ":
-          encrypted_text += char
-     else: 
-         index = alphabet.find(char)
-         new_index = (index + shift) % len(alphabet)
-         encrypted_text +=alphabet[new_index]
-print(f"Plain Text: {myName}")
-print(f"char: {char} encrypted text: {encrypted_text}")
+def caesar():
+    for char in myName.lower():
+         if char == " ":
+              encrypted_text += char
+         else: 
+             index = alphabet.find(char)
+             new_index = (index + shift) % len(alphabet)
+             encrypted_text +=alphabet[new_index]
+    print(f"Plain Text: {myName}")
+    print(f"char: {char} encrypted text: {encrypted_text}")
